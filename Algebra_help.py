bcd561e2a33131700c2718e695faa0935c466eed
christina <clh@cholland.me>
2021-05-25 09:53:13 -0400

    working on debugging the starting from vertex form section

--- Algebra_help.py
+++ Algebra_help.py
@@ -2156,9 +2156,9 @@ elif eqn_type == 'Quadratic Equations':
                 if a!=1:
                     st.write('Step 2: Divide by "a".')
                     options = list({-1*k/a, k/a, -1*k/h, k/h, -1*h/a, h/a})
-                    options = [np.round(c,10) for c in options]
                     options = [c if c!=0 else 0.0 for c in options]
                     options = [int(c) if c%1==0 else np.round(c,4) for c in options]
+                    st.write(options)
                     new_rhs = options[0]
                     options.sort()
                     new_rhs_in = st.selectbox('What is the right hand side equal to now?',['SELECT'] + options)
